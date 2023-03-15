34284384955df7d0adde8fc7cf2b8390220c14e6
PRAKTIKAN@0605-PC-21.(none) <PRAKTIKAN@0605-PC-21.(none)>
2023-03-15 17:13:50 +0700

    menambahkan class KodeBuah

---
+++ modul4_1302210128/PosisiKarakterGame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*namespace modul4_1302210128
+{
+    internal class PosisiKarakterGame
+    {
+
+        public enum State { JONGKOK, TENGKURAP, TERBANG, BERDIRI };
+        public enum button { W, S, X};
+
+        public State prevstate;
+        public State nextstate;
+        public button tombol;
+        
+        public gerak(State prevstate, State nextstate, State tombol)
+        {
+            this.prevstate = prevstate; 
+            this.nextstate = nextstate;
+            this.tombol = (button)tombol;
+        }
+
+        
+    }
+}*/
--- modul4_1302210128/Program.cs
+++ modul4_1302210128/Program.cs
@@ -5,18 +5,21 @@ using static modul4_1302210128.kodebuah;
 
 
 public class program
-{static void Main()
-{
-   
-    Console.WriteLine(kodebuah.getkodebuah(buah.Apple));
-}
-/*
-    enum State {START, GAME, PAUSE, EXIT};
-    public static AppDomain ()
+    {static void Main()
     {
-        State state = State.START;
-        string[] screenName = { "START", "GAME", "PAUSE", "EXIT" };
-    }*/
+        Console.WriteLine(kodebuah.getkodebuah(buah.Apple));
+        Console.WriteLine(kodebuah.getkodebuah(buah.Aprikot));
+        Console.WriteLine(kodebuah.getkodebuah(buah.Alpukat));
+        Console.WriteLine(kodebuah.getkodebuah(buah.Pisang));
+        Console.WriteLine(kodebuah.getkodebuah(buah.Paprika));
+        Console.WriteLine(kodebuah.getkodebuah(buah.Blackberry));
+        Console.WriteLine(kodebuah.getkodebuah(buah.Ceri));
+        Console.WriteLine(kodebuah.getkodebuah(buah.Kelapa));
+        Console.WriteLine(kodebuah.getkodebuah(buah.Jagung));
+
+    }
+
+
 }
 
 
