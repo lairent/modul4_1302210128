5d6f3378b1fe4aa184b718b6fc84192b120b88b0
PRAKTIKAN@0605-PC-21.(none) <PRAKTIKAN@0605-PC-21.(none)>
2023-03-15 16:38:46 +0700

    com1

--- modul4_1302210128/Program.cs
+++ modul4_1302210128/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul4_1302210128;
+using static modul4_1302210128.kodebuah;
+
+
+
+public class program
+{static void Main()
+{
+   
+    Console.WriteLine(kodebuah.getkodebuah(buah.Apple));
+}
+/*
+    enum State {START, GAME, PAUSE, EXIT};
+    public static AppDomain ()
+    {
+        State state = State.START;
+        string[] screenName = { "START", "GAME", "PAUSE", "EXIT" };
+    }*/
+}
+
+
+
---
+++ modul4_1302210128/kodebuah.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302210128
+{
+    internal class kodebuah
+    {
+        public enum buah
+        {
+            Apple,
+            Aprikot,
+            Alpukat,
+            Pisang,
+            Paprika,
+            Blackberry,
+            Ceri,
+            Kelapa,
+            Jagung
+        }
+
+        public static string getkodebuah(buah nama)
+        {
+            string[] kodebuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00" };
+
+            return kodebuah[(int)nama];
+        }
+
+    }
+}
